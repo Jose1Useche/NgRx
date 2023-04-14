64e2e29b7fdcffd1e2a0fab289858b0517eeba56
Jose Useche <jose1useche@gmail.com>
2023-04-14 14:44:48 -0400

    Inicio configurado NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,    
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/show-case/page/show-case-page/show-case-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ItemModel } from 'src/app/core/models/item.interface';
+import { ShowCaseService } from '../../services/show-case.service';
+
+@Component({
+  selector: 'app-show-case-page',
+  templateUrl: './show-case-page.component.html',
+  styleUrls: ['./show-case-page.component.css']
+})
+export class ShowCasePageComponent implements OnInit {
+
+  listItems: ItemModel[] = []
+  listItems$: Observable<any> = new Observable()
+
+  constructor(private showCase: ShowCaseService) {}
+
+  ngOnInit(): void {
+    this.loadData()
+  }
+
+  loadData(): void {
+
+  }
+
+}
